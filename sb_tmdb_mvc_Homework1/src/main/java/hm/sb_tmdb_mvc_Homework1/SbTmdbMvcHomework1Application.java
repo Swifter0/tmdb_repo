055e118cf0f68b07351e0493a320d802634b805e
Swifter0 <167337586+Swifter0@users.noreply.github.com>
2024-04-18 14:51:10 +0200

    Project start

---
+++ sb_tmdb_mvc_Homework1/src/main/java/hm/sb_tmdb_mvc_Homework1/SbTmdbMvcHomework1Application.java
@@ -0,0 +1,13 @@
+package hm.sb_tmdb_mvc_Homework1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbTmdbMvcHomework1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbTmdbMvcHomework1Application.class, args);
+	}
+
+}
